1c0a073aa641a240551ec5be75427b5d96221953
Roksana Dzioba <roksanadzioba@gmail.com>
2020-11-10 23:30:17 +0100

    add header file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const sections = ['Home', 'About', 'Contact', 'Services']
+    const navLinks = sections.map(section => {
+      return(
+        <li><a href={'#'+section}>{section}</a></li>
+      )
+    });
+    return (
+      <div className="wrapper">
+        <nav>
+          <h2>{this.props.logoTitle}</h2>
+          <ul>
+            {navLinks}
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    const bacgroundImage =
+    render() {
+        return(
+            <h3>xd</h3>
+        );
+    }
+}
+
+export default Header;
