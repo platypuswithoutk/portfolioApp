98edb67936f50881f11d1084bba4f467b209a7ab
Roksana Dzioba <roksanadzioba@gmail.com>
2020-11-13 06:52:03 +0100

    Header is ready

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const backgroundImageStyle = {
     backgroundImage : `url(${Background})`,
     height: '81vh',
     backgroundSize: "cover",
-    backgroundPosition: 'center'
+    backgroundPosition: 'center',
     //backgroundRepeat : 'no-repeat', 
 } 
 
@@ -14,11 +14,12 @@ class Header extends React.Component {
     render() {
         return(
             <header style={backgroundImageStyle}>
-                <div className="xd">
+                <div className="header-style">
                     <h1>{this.props.title}</h1>
                     <p>SEO/SEM Agency</p>
                     <a href="#btn">{this.props.button}</a>
                 </div>
+                
             </header>
         );
     }
